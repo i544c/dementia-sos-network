81b9b6f2a7a4427283943ca646c5ef4fc095f430
i544c <leo@i544c.me>
2017-05-25 17:04:15 +0900

    Add accounts

--- dsn/urls.py
+++ dsn/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^missing/', include('missing.urls')),
     url(r'^admin/', admin.site.urls),
 ]
