6fc0fe409e8d5869f73d6c345c5073a2f2f0dfa2
i544c <leo@i544c.me>
2017-05-25 19:34:25 +0900

    Add navbar

--- dsn/urls.py
+++ dsn/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^accounts/', include('accounts.urls')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^missing/', include('missing.urls', namespace='missing')),
     url(r'^admin/', admin.site.urls),
 ]
